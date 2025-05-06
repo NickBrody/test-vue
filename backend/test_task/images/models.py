3cf5535ed2fe7c92163c6e159976e75fd0919718
Nick <deliciousman99@gmail.com>
2025-05-06 22:51:43 +0900

    first commit

---
+++ backend/test_task/images/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Image(models.Model):
+    image_base64 = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
